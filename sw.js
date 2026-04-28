88881f77fdc2b2e4ae5532a8b9b15687dd4a6ae1
planificacionfabiana-netizen <planificacionfabiana@gmail.com>
2026-04-27 23:00:50 -0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Service Worker básico para permitir la instalación
+self.addEventListener('fetch', function(event) {
+    // Aquí podrías programar que el chat funcione sin internet, 
+    // pero por ahora lo dejamos pasar para que sea quirúrgico.
+});
